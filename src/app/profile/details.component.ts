e5ce5643b66fabe3b8b4bfcedb6d49a961700f1e
Jodel Saragena <jodelsaragena19@gmail.com>
2024-05-16 12:13:09 +0800

    Profile

--- src/app/profile/details.component.ts
+++ src/app/profile/details.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { AccountService } from '@app/_services';
 
-@Component({ templateUrl: 'details.component.html' })
+@Component({
+  selector: 'app-details',
+  templateUrl: 'details.component.html',
+})
 export class DetailsComponent {
-    account = this.accountService.accountValue;
+  account = this.accountService.accountValue;
+  profileImage: SafeUrl;
+  tempProfileImage: SafeUrl;
 
-    constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private sanitizer: DomSanitizer) {}
+
+  onFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.tempProfileImage = this.sanitizer.bypassSecurityTrustUrl(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  saveProfileImage(): void {
+    this.profileImage = this.tempProfileImage;
+  }
 }
